6c2213be42fd76872b1a8ae52d85167bf12dbe89
Bhavik Bhagat <230625506+bhavik-bhagat-smu@users.noreply.github.com>
2025-11-06 00:15:03 -0400

    add: experiment.sql and result screenshots

---
+++ as3_data+scripts/experiment.sql
@@ -0,0 +1,24 @@
+SET PROFILING=1;
+
+-- default query
+BEGIN;
+select lname, fname from person, history as H where _id=pid and eyear=2000 and H.city='Las Vegas';
+ROLLBACK;
+
+-- insert query (from phistory2.tsv) with 1000 records
+BEGIN;
+load data local infile 'phistory2.tsv' into table history;
+ROLLBACK;
+
+-- update query with city
+BEGIN;
+DELETE FROM history WHERE city="Las Vegas";
+ROLLBACK;
+
+
+-- delete query with country
+BEGIN;
+DELETE FROM history WHERE country="Norway";
+ROLLBACK;
+
+SET PROFILING=0;
